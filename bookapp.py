c277e1f6e08e4aae269c573612d1656ca540d47f
Jean Ruggiero <jeanruggiero@gmail.com>
2019-05-20 15:58:10 -0700

    Lesson 4 cgi and wsgi exercises complete

--- bookapp.py
+++ bookapp.py
@@ -1,23 +1,67 @@
-import re
-
 from bookdb import BookDB
+import traceback
 
 DB = BookDB()
 
 
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
+    info = DB.title_info(book_id)
+    if info is None:
+        raise NameError
+    response_body = f"<h1>{info['title']}</h1>\n" + \
+        f"<p><b>Author</b>: {info['author']}<br>" + \
+        f"<b>Publisher</b>: {info['publisher']}<br>" + \
+        f"<b>ISBN</b>: {info['isbn']}<br>" + \
+        f"<a href='/'>Back to the list</a></p>"
+    return response_body
 
 
 def books():
-    return "<h1>a list of books</h1>"
+    response_body = ["<h2>My Bookshelf</h2>", "<ul>"]
+    for title in DB.titles():
+        response_body.append(f"<li><a href=/book/{title['id']}>{title['title']}</a></li>")
+    response_body.append("</ul>")
+    return '\n'.join(response_body)
+
+
+def resolve_path(path):
+    funcs = {
+        '': books,
+        'book': book,
+    }
+
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
+    return func, args
 
 
 def application(environ, start_response):
-    status = "200 OK"
     headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+
+    try:
+        path = environ.get("PATH_INFO", None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(("Content-length", str(len(body))))
+        start_response(status, headers)
+        return [body.encode()]
 
 
 if __name__ == '__main__':
